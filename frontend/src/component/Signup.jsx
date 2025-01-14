3bc4dee7c1d5eda72585e83264323be5ca0eced8
vikashi3 <infosoftvikash@gmail.com>
2025-01-14 18:48:11 +0530

    url related bugs fixed in profile section

--- frontend/src/component/Signup.jsx
+++ frontend/src/component/Signup.jsx
@@ -34,21 +34,20 @@ function Signup() {
           },
         }
       );
+
       const token = response.data;
-      // console.log(token);
       localStorage.setItem("token", token);
-      toast.success("successfully Registered");
+      toast.success("Successfully Registered");
       navigate("/profile");
     } catch (error) {
-      if (error.response.status === 400) {
+      if (error.response?.status === 400) {
         toast.warning("No file uploaded");
-      }
-      if (error.response.status === 409) {
+      } else if (error.response?.status === 409) {
         toast.warning("User already registered");
       } else {
         toast.error("Internal server error");
       }
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -56,21 +55,29 @@ function Signup() {
     setInput({ ...input, profilePic: e.target.files[0] });
   };
 
+  const handleInputChange = (e, key) => {
+    setInput({ ...input, [key]: e.target.value });
+  };
+
+  const handleBlur = (e, key) => {
+    setInput({ ...input, [key]: e.target.value.trim() });
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-900">
-      <div className="max-w-md mx-4 px-2 bg-slate-100 shadow-lg rounded-lg overflow-hidden">
+      <div className="max-w-lg mx-4 px-2 bg-slate-100 shadow-lg rounded-lg overflow-hidden">
         <form
           onSubmit={handleSubmit}
           encType="multipart/form-data"
           className="block px-4 py-4 mb-2 text-sm text-gray-700"
-          >
+        >
           <label
             htmlFor="profile"
             className="block mb-2 text-sm font-bold text-gray-700"
           >
             Profile Picture
           </label>
-          <input type="file" onChange={handleFileChange} name="file"/>
+          <input type="file" onChange={handleFileChange} name="file" />
 
           <label
             htmlFor="name"
@@ -81,8 +88,8 @@ function Signup() {
           <input
             type="text"
             value={input.name}
-            onChange={(e) => setInput({ ...input, name: e.target.value })}
-            onBlur={(e)=> setInput({ ...input, name: e.target.value }).trim()}
+            onChange={(e) => handleInputChange(e, "name")}
+            onBlur={(e) => handleBlur(e, "name")}
             placeholder="Name"
             required
             className="w-full px-3 py-2 mb-3 border rounded-md focus:outline-none focus:border-blue-500 capitalize"
@@ -97,8 +104,8 @@ function Signup() {
           <input
             type="email"
             value={input.email}
-            onChange={(e) => setInput({ ...input, email: e.target.value })}
-            onBlur={(e)=> setInput({ ...input, email: e.target.value.trim()})}
+            onChange={(e) => handleInputChange(e, "email")}
+            onBlur={(e) => handleBlur(e, "email")}
             placeholder="Email"
             required
             className="w-full px-3 py-2 mb-3 border rounded-md focus:outline-none focus:border-blue-500"
@@ -113,8 +120,8 @@ function Signup() {
           <input
             type="password"
             value={input.password}
-            onChange={(e) => setInput({ ...input, password: e.target.value })}
-            onBlur={(e)=> setInput({ ...input, password: e.target.value.trim()})}
+            onChange={(e) => handleInputChange(e, "password")}
+            onBlur={(e) => handleBlur(e, "password")}
             placeholder="Password"
             required
             className="w-full px-3 py-2 mb-3 border rounded-md focus:outline-none focus:border-blue-500"
@@ -129,8 +136,8 @@ function Signup() {
           <input
             type="tel"
             value={input.contact}
-            onChange={(e) => setInput({ ...input, contact: e.target.value })}
-            onBlur={(e)=> setInput({ ...input, contact: e.target.value.trim()})}
+            onChange={(e) => handleInputChange(e, "contact")}
+            onBlur={(e) => handleBlur(e, "contact")}
             placeholder="Contact"
             required
             className="w-full px-3 py-2 mb-6 border rounded-md focus:outline-none focus:border-blue-500"
@@ -144,11 +151,12 @@ function Signup() {
         </form>
         <div className="flex flex-col items-center">
           <div className="flex gap-4 w-full justify-center pb-2">
-          <span>Already have an Account?</span>
-            <Link to="/login" className="hover:text-green-600 text-blue-500"> Login</Link>
+            <span>Already have an Account?</span>
+            <Link to="/login" className="hover:text-green-600 text-blue-500">
+              Login
+            </Link>
           </div>
           <Link to="/" className="text-blue-600 mb-5 hover:text-red-600">
-            {" "}
             Home
           </Link>
         </div>
